1597e1e5daf67e8319e583519f3401c607cb97c0
itsyannic <s6yakrus@uni-bonn.de>
2024-04-29 15:30:37 +0200

    added files

---
+++ python/SchwingerDyson.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class SchwingerDyson:
+
+    def __init__(self, beta, q, J, discretization, steps=1000):
+
+        self.q = q
+        self.beta = beta
+        self.J = J
+        self.discretization = discretization
+        self.steps = steps
+
+        self.G33n = np.array((2*discretization, 2*discretization))
+        self.G33d = np.array((2*discretization, 2*discretization))
+        self.Ghatn = np.array((4*discretization, 4*discretization))
+        self.Ghatd = np.array((4*discretization, 4*discretization))
+
+        self.init_matrices()
+
+    def init_matrices(self):
+
+        for i in range(2*self.discretization):
+            for j in range(2*self.discretization):
+                self.G33n[i,j] = 0.5*np.sign(i-j)
+
+                if ((i<self.discretization and j<self.discretization) or (i >= self.discretization and j>=self.discretization )):
+                    self.G33d[i,j] = 0.5*np.sign(i-j)
+                else:
+                    self.G33d[i,j] = 0
+
+        for i in range(4*self.discretization):
+            for j in range(4*self.discretization):
+
+                if ( ((self.discretization<=i<3*self.discretization) and (self.discretization<=j<3*self.discretization)) or (( i< self.discretization or 3*self.discretization<=i<4*self.discretization) and ( j< self.discretization or 3*self.discretization<=j<4*self.discretization))):
+                    self.Ghatn[i,j] = 0.5*np.sign(i-j)
+                else:
+                    self.Ghatn[i,j] = 0
+
+                if ((i<2*self.discretization and j<2*self.discretization) or (i >= 2*self.discretization and j>=2*self.discretization )):
+                    self.Ghatd[i,j] = 0.5*np.sign(i-j)
+                else:
+                    self.Ghatd[i,j] = 0
+
+
+    def solve(self):
+
+        return
+
---
+++ python/main.py
@@ -0,0 +1,6 @@
+import numpy
+import SchwingerDyson
+
+sd = SchwingerDyson.SchwingerDyson(1,1,1,10)
+
+print(sd.Ghatn)
