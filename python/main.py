b603b4cc284eea0b0739a90bd6c98cc8a70b0331
Saurabh <saurabh@saurabhs-air.localdomain>
2024-05-10 15:11:06 +0200

    test run for main file

--- python/main.py
+++ python/main.py
@@ -2,8 +2,8 @@ import numpy
 import SchwingerDyson
 import fields
 
-sd = SchwingerDyson.SchwingerDyson(1,1,1,0,10)
+sd = SchwingerDyson.SchwingerDyson(1,1,1,0,4)
 
-print(fields.convert_map_to_matrix(fields.G11,sd.Ghatn,10))
+print(fields.convert_map_to_matrix(fields.G11,sd.Ghatn,2))
 
-print(sd.Ghatn)
+#print(sd.Ghatn)
