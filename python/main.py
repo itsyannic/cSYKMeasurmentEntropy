dd6a23e97be19b8da049827ad94fc65437ef81f8
itsyannic <s6yakrus@uni-bonn.de>
2024-05-17 17:27:41 +0200

    changed main

--- python/main.py
+++ python/main.py
@@ -2,8 +2,7 @@ import numpy as np
 import SchwingerDyson
 import fields
 
-#sd = SchwingerDyson.SchwingerDyson(100,8,1,0,100,0.0001)
-sd = SchwingerDyson.SchwingerDyson(100,8,1,0,4,0.0001)
+sd = SchwingerDyson.SchwingerDyson(100,8,1,0,500,0.000000001)
 sd.solve()
 
 print(np.trace(sd.Ghatn))
