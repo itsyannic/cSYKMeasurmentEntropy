90816131111ac126af54962a5dc5b10502154f52
itsyannic <s6yakrus@uni-bonn.de>
2024-05-21 12:03:49 +0200

    cosmetic changes

--- python/main.py
+++ python/main.py
@@ -1,8 +1,7 @@
 import numpy as np
-import SchwingerDyson
-import fields
+from SchwingerDyson import SchwingerDyson
 
-sd = SchwingerDyson.SchwingerDyson(100,8,1,0,500,0.000000001)
+sd = SchwingerDyson(100,8,1,0,500,0.000000001)
 sd.solve()
 
 print(np.trace(sd.Ghatn))
