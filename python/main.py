70e0211ab27c90d7859c9647465dd2affc77ecd7
itsyannic <s6yakrus@uni-bonn.de>
2024-07-19 15:12:52 +0200

    still no progress

--- python/main.py
+++ python/main.py
@@ -7,8 +7,8 @@ import JT_entropy
 
 #set plot parameters
 ms = np.linspace(0,1,10,endpoint=False, dtype=np.double)
-q = 8
-beta = 100
+q = 4
+beta = 50
 J = 1
 
 if (True):
@@ -16,7 +16,7 @@ if (True):
     results = []
 
     for m in ms:
-        sd = SchwingerDyson(beta,q,J,m,100,0.00001,weight=0.5,max_iter=10000)
+        sd = SchwingerDyson(beta,q,J,m,200,0.00001,weight=0.5,max_iter=10000)
         sd.solve()
         results.append(physics.results(sd))
         print(m)
@@ -37,12 +37,14 @@ else:
 
 m = [point['m'] for point in results]
 I = [point['renyi2'] for point in results]
-#JT = [JT_entropy.S_gen(point['charge'],point['m'],q,beta,J) for point in results]
+x = np.linspace(0,1,50, endpoint=False)
+JT = [JT_entropy.S_gen(0,m,q,beta,J) for m in x]
 
 
 plt.scatter(m,I)
-#plt.plot(m,JT)
+plt.plot(x,JT)
 plt.xlabel('m')
 plt.ylabel('Renyi-2 Entropy I2')
 plt.title('beta = ' + str(beta) + ', q=' + str(q))
+
 plt.show()
