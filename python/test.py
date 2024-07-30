683b2e5804beef7759c21e5fff8c5d7e2be09aa6
itsyannic <s6yakrus@uni-bonn.de>
2024-07-30 15:24:31 +0200

    -

--- python/test.py
+++ python/test.py
@@ -3,20 +3,21 @@ from matplotlib import pyplot as plt
 from SchwingerDyson import SchwingerDyson
 import fields
 
-beta = 20
+beta = 100
 q = 4
-J = 0
+J = 1
 N = 100
 L = 0.000001
 
 sd = SchwingerDyson(beta,q,J,1,N,L,weight=0.5,max_iter=5000, silent=True)
 sd.solve()
-Gdij = fields.read_G_from_Ghat(sd.Ghatd, int(sd.discretization/2))
+Gdij = fields.read_G_from_Ghat(sd.Ghatn, int(sd.discretization/2))
 Gm = Gdij['G11']
 Gmt = Gdij['G11'].transpose()
+#print(sd.Ghatn)
 #print(Gm)
 
 tau1 = 0
-tau2 = np.linspace(-beta,beta,2*N)
-plt.plot(tau2,np.concatenate((-Gm[tau1][0:N], Gm[tau1][0:N])))
+tau2 = np.linspace(-2*beta,2*beta,4*N)
+plt.plot(tau2,np.concatenate((-Gm[tau1], Gm[tau1])))
 plt.show()
