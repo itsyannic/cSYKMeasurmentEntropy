bbc2629d5885329cdaaec23bb2a538cca6065b19
itsyannic <s6yakrus@uni-bonn.de>
2024-07-30 15:28:08 +0200

    -

--- python/test.py
+++ python/test.py
@@ -5,15 +5,16 @@ import fields
 
 beta = 20
 q = 4
-J = 0
-N = 80
+J = 1
+N = 100
 L = 0.000001
 
 sd = SchwingerDyson(beta,q,J,1,N,L,weight=0.5,max_iter=5000, silent=True)
+sd.solve()
 Gdij = fields.read_G_from_Ghat(sd.Ghatn, int(sd.discretization/2))
 Gm = Gdij['G11'] + Gdij['G22'] 
 Gmt = Gm.transpose()
 
 tau = np.linspace(-2*beta,2*beta,4*N)
-plt.plot(tau,np.concatenate((np.flip(Gmt[20]),Gm[20])))
+plt.plot(tau,np.concatenate((-Gm[0],Gm[0])))
 plt.show()
