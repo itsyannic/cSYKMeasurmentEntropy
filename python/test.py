73011c07ef0f5f8614293693be19b2e656731af4
itsyannic <s6yakrus@uni-bonn.de>
2024-07-31 16:42:12 +0200

    Ghatn and Ghatd differ by 2log2

--- python/test.py
+++ python/test.py
@@ -9,8 +9,10 @@ J = 0
 N = 100
 L = 0.000001
 
-sd = SchwingerDyson(beta,q,J,0,N,L,weight=0.5,max_iter=5000, silent=True)
+sd = SchwingerDyson(beta,q,J,1,N,L,weight=0.5,max_iter=5000, silent=True)
 sd.solve()
+print(np.log(np.linalg.det(sd.Ghat_n_free_inverse))-np.log(np.linalg.det(sd.Ghat_d_free_inverse)))
+print(np.log(np.linalg.det(sd.G33_n_free_inverse))-np.log(np.linalg.det(sd.G33_d_free_inverse)))
 Gdij = fields.read_G_from_Ghat(sd.Ghatn, int(sd.discretization/2))
 Gm = Gdij['G11']
 #Gm = sd.G33n
