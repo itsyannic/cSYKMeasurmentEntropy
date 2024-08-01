579ae5535c8abe58bfba46be3561e534e2a7eb07
itsyannic <s6yakrus@uni-bonn.de>
2024-08-01 11:27:45 +0200

    small changes to test

--- python/test.py
+++ python/test.py
@@ -4,10 +4,10 @@ from SchwingerDyson import SchwingerDyson
 import fields
 import physics
 
-beta = 10
+beta = 1
 q = 4
 J = 1
-N = 500
+N = 100
 L = 0.000001
 
 sd = SchwingerDyson(beta,q,J,1,N,L,weight=0.5,max_iter=5000, silent=True)
@@ -15,6 +15,8 @@ sd.solve()
 #print(np.log(np.linalg.det(sd.Ghat_n_free_inverse))-np.log(np.linalg.det(sd.Ghat_d_free_inverse)))
 #print(np.log(np.linalg.det(sd.G33_n_free_inverse))-np.log(np.linalg.det(sd.G33_d_free_inverse)))
 results = physics.results(sd)
+print(results['Iden'])
+print(results['Inum'])
 print(results['renyi2'])
 Gdij = fields.read_G_from_Ghat(sd.Ghatn, int(sd.discretization/2))
 Gm = Gdij['G11']
