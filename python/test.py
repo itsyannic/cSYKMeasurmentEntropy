b785d494186d7f87ce9411c862b0e39febed9695
itsyannic <s6yakrus@uni-bonn.de>
2024-08-05 10:52:14 +0200

    changes to test

--- python/test.py
+++ python/test.py
@@ -4,16 +4,16 @@ from SchwingerDyson import SchwingerDyson
 import fields
 import physics
 
-beta = 1
+beta = 1000
 q = 4
-J = 1
+J = 0
 N = 100
 L = 0.000001
 
 sd = SchwingerDyson(beta,q,J,1,N,L,weight=0.5,max_iter=5000, silent=True)
 sd.solve()
-#print(np.log(np.linalg.det(sd.Ghat_n_free_inverse))-np.log(np.linalg.det(sd.Ghat_d_free_inverse)))
-#print(np.log(np.linalg.det(sd.G33_n_free_inverse))-np.log(np.linalg.det(sd.G33_d_free_inverse)))
+print(np.log(np.linalg.det(sd.Ghat_n_free_inverse))-np.log(np.linalg.det(sd.Ghat_d_free_inverse)))
+print(np.log(np.linalg.det(sd.G33_n_free_inverse))-np.log(np.linalg.det(sd.G33_d_free_inverse)))
 results = physics.results(sd)
 print(results['Iden'])
 print(results['Inum'])
@@ -26,4 +26,4 @@ Gmt = Gdij['G11'].transpose()
 tau1 = 0
 tau2 = np.linspace(-2*beta,2*beta,4*N)
 plt.plot(tau2,np.concatenate((-Gm[tau1], Gm[tau1])))
-#plt.show()
+plt.show()
